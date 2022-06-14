a2797c6d71f7a3ca637ac095841fabf74bafd8bf
alym karim <akarim@openstudycollege.com>
2022-06-14 09:13:06 +0100

    fix: generate server-ca.pem everytime prisma is initialized

--- app/db.server.ts
+++ app/db.server.ts
@@ -5,7 +5,6 @@ let prisma: PrismaClient;
 declare global {
     var __db__: PrismaClient;
 }
-
 // this is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
