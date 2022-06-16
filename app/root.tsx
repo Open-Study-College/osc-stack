04821226111b4fe0237f636fa61e81e3d1b5c3a2
alym karim <akarim@openstudycollege.com>
2022-06-16 09:40:48 +0100

    Revert "Merge pull request #12 from Open-Study-College/feature/stylelint-eslint-prettier-browserlist-nvm"
    This reverts commit 818939ccdd25fde932e38d24b259958f68b43e90, reversing
    changes made to cfb29a42e01ae89fff81277292dd33edc11ca724.

--- app/root.tsx
+++ app/root.tsx
@@ -1,70 +1,54 @@
-import type { LinksFunction, LoaderFunction, MetaFunction } from '@remix-run/node';
-import { json } from '@remix-run/node';
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
-import { ChakraProvider } from '@chakra-ui/react';
-import { withEmotionCache } from '@emotion/react';
-import type { EmotionCache } from '@emotion/react';
-import { useEmotionCache } from './hooks/useEmotionCache';
-import DOMPurify from 'isomorphic-dompurify';
+import type {
+  LinksFunction,
+  LoaderFunction,
+  MetaFunction,
+} from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
 
-import tailwindStylesheetUrl from './styles/tailwind.css';
-import { getUser } from './session.server';
+import tailwindStylesheetUrl from "./styles/tailwind.css";
+import { getUser } from "./session.server";
 
 export const links: LinksFunction = () => {
-    return [{ rel: 'stylesheet', href: tailwindStylesheetUrl }];
+  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
 };
 
 export const meta: MetaFunction = () => ({
-    charset: 'utf-8',
-    title: 'Remix Notes',
-    viewport: 'width=device-width,initial-scale=1'
+  charset: "utf-8",
+  title: "Remix Notes",
+  viewport: "width=device-width,initial-scale=1",
 });
 
 type LoaderData = {
-    user: Awaited<ReturnType<typeof getUser>>;
+  user: Awaited<ReturnType<typeof getUser>>;
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
-    return json<LoaderData>({
-        user: await getUser(request)
-    });
+  return json<LoaderData>({
+    user: await getUser(request),
+  });
 };
 
-interface DocumentProps {
-    children: React.ReactNode;
-}
-
-const Document = withEmotionCache(({ children }: DocumentProps, emotionCache: EmotionCache) => {
-    const serverStyleData = useEmotionCache(emotionCache);
-    return (
-        <html lang="en" className="h-full">
-            <head>
-                <Links />
-                <Meta />
-                {serverStyleData.map(({ key, ids, css }) => (
-                    <style
-                        key={key}
-                        data-emotion={`${key} ${ids.join(' ')}`}
-                        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(css) }}
-                    />
-                ))}
-            </head>
-            <body>
-                {children}
-                <ScrollRestoration />
-                <Scripts />
-                <LiveReload />
-            </body>
-        </html>
-    );
-});
-
 export default function App() {
-    return (
-        <Document>
-            <ChakraProvider>
-                <Outlet />
-            </ChakraProvider>
-        </Document>
-    );
+  return (
+    <html lang="en" className="h-full">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-full">
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
 }
