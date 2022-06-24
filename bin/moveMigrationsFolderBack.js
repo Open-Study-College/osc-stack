4bc84a62b1444211e11a390cc9cd506fc3e45bb5
alym karim <akarim@openstudycollege.com>
2022-06-24 11:18:32 +0100

    fix: reset migrations folder

--- bin/moveMigrationsFolderBack.js
+++ bin/moveMigrationsFolderBack.js
@@ -6,17 +6,16 @@ var oldPath =  path.join(process.cwd(), '/migrations')
 var newPath = path.join(process.cwd(), '/prisma/migrations')
 
 try {
+    fs.readFile(path.join(process.cwd(), '/prisma/schema.prisma'), 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        var result = data.replace(/sqlite/g, 'mysql');
 
-        fs.readFile(path.join(process.cwd(), '/prisma/schema.prisma'), 'utf8', function (err,data) {
-            if (err) {
-                return console.log(err);
-            }
-            var result = data.replace(/sqlite/g, 'mysql');
-
-            fs.writeFile(path.join(process.cwd(), '/prisma/schema.prisma'), result, 'utf8', function (err) {
-                if (err) return console.log(err);
-            });
+        fs.writeFile(path.join(process.cwd(), '/prisma/schema.prisma'), result, 'utf8', function (err) {
+            if (err) return console.log(err);
         });
+    });
 
   if (fs.existsSync(oldPath)) {
         fs.rmSync(path.join(process.cwd(), '/prisma/migrations'), { recursive: true, force: true });
