c42c09d397c34c6f2bd69f824d434ff385361ac7
Simon Shahriveri <si@hi-im-si.com>
2022-07-12 16:09:18 +0100

    feat: updates all styles to utilise new sass methods

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -7,6 +7,13 @@ export default function Index() {
   return (
     <div>
       <h1 className="text-slate-100">This is the index page</h1>
+      <div className="o-ar  o-ar--1by1">
+        <img
+          className="o-ar__item"
+          src="https://images.unsplash.com/photo-1655071516487-03bb9267b1f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2938&q=80"
+          alt=""
+        />
+      </div>
       <Form action="/logout" method="post">
         <button
           type="submit"
