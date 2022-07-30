fd6a7ea935bea9274645b3500ae8a51bc721592c
alym karim <akarim@openstudycollege.com>
2022-07-30 16:10:33 +0100

    fix: raw output is always pscale password list

--- .pscale/cli-helper-scripts/create-branch-connection-string.sh
+++ .pscale/cli-helper-scripts/create-branch-connection-string.sh
@@ -14,7 +14,7 @@ function create-branch-connection-string {
         exit 1
     fi
 
-    local output=`echo $raw_output | jq -r "[.[] | select(.display_name == \"$CREDS\") ] | .[0].id "`
+    local output=`echo $raw_output
     # if output is not "null", then password exists, delete it
     if [ "$output" != "null" ]; then
         echo "Deleting existing password $output"
