c6d62df833b76c51705c922551915812081cea15
alym karim <akarim@openstudycollege.com>
2022-07-31 14:13:24 +0100

    fix: cred to variabel

--- .pscale/cli-helper-scripts/create-db-branch-dr-and-connection.sh
+++ .pscale/cli-helper-scripts/create-db-branch-dr-and-connection.sh
@@ -17,7 +17,7 @@ create-deploy-request "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME"
 
 
 . create-branch-connection-string.sh
-create-branch-connection-string "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "creds-${BRANCH_NAME}" "sharesecret"
+create-branch-connection-string "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "test-cred" "sharesecret"
     # if $2 and $3 are set, generate secret output links
     if [ -n "$2" ] && [ -n "$3" ]; then
         for i in `seq 1 $2`; do
