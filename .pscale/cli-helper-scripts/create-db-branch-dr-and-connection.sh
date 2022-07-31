943f0bb04ef9d45bf1842472a1fe2a489172d033
alym karim <akarim@openstudycollege.com>
2022-07-31 14:02:48 +0100

    fix: move secret _output_links_generating step to first function

--- .pscale/cli-helper-scripts/create-db-branch-dr-and-connection.sh
+++ .pscale/cli-helper-scripts/create-db-branch-dr-and-connection.sh
@@ -18,3 +18,11 @@ create-deploy-request "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME"
 
 . create-branch-connection-string.sh
 create-branch-connection-string "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "creds-${BRANCH_NAME}" "sharesecret"
+    # if $2 and $3 are set, generate secret output links
+    if [ -n "$2" ] && [ -n "$3" ]; then
+        for i in `seq 1 $2`; do
+            for j in `seq 1 $3`; do
+                echo "::set-output name=dbconnection_${i}_${j}::`curl -s -X POST -d "plain&secret=$MY_DB_URL" https://shared-secrets-planetscale.herokuapp.com/`"          
+            done
+        done
+    fi
