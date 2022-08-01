a8144491684be8678f8aeea2469f4584e56cde3c
alym karim <akarim@openstudycollege.com>
2022-08-01 21:37:07 +0100

    fix: added dir to restore dump

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,2 +1,2 @@
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME"
-pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME"
+pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir .
