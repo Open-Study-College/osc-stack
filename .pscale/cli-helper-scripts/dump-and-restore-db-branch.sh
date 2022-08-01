282309b56b99fc1f8c0cfa6802ed56807f890738
alym karim <akarim@openstudycollege.com>
2022-08-01 21:20:45 +0100

    fix: removed local from bash script dump-and-restore

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,7 +1,2 @@
-local DB_NAME=$1
-local BRANCH_NAME=$2
-local ORG_NAME=$3
-local recreate_branch=$4
-
 pscale database dump "$DB_NAME" "main"
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables
