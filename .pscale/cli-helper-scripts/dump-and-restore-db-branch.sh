ef95415210078cd1b853663d9f91b52fa4a3c05c
alym karim <akarim@openstudycollege.com>
2022-08-02 17:23:17 +0100

    fix: pass root dir from yml file

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-DIR="$pwd"
 echo "Script location: ${DIR}"
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output "$DIR"
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "$DIR"
