542804d42052116c6874de9a220ef97d37b54091
alym karim <akarim@openstudycollege.com>
2022-08-02 09:15:44 +0100

    fix: remove mkdir script

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-mkdir pscale_dump_"$DB_NAME"_"$BRANCH_NAME"
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME"
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "pscale_dump_\"$DB_NAME\"_\"$BRANCH_NAME\""
