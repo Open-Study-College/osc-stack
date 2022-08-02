a8d3e7835d874469c24201abaff1b2a9af3afd39
alym karim <akarim@openstudycollege.com>
2022-08-02 10:45:32 +0100

    fix: syntax for restore dump

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME"
-pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "pscale_dump_${DB_NAME}_main"
+pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "pscale_dump_$DB_NAME_main"
