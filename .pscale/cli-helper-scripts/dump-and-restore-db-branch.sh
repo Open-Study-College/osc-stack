f85dedac0296e84de0ff528c92f71d7064887732
alym karim <akarim@openstudycollege.com>
2022-08-02 14:07:52 +0100

    fix: change current directory to home

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-sudo bash pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output ".pscale/cli-helper-scripts"
-sudo bash pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir ".pscale/cli-helper-scripts/pscale_dump_osc-academic_main"
+cd $HOME
+pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output ".pscale/cli-helper-scripts"
+pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir ".pscale/cli-helper-scripts/pscale_dump_osc-academic_main"
