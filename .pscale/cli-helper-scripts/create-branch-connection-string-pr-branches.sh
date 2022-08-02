136823e4488362c0a3c1fc0ff1ecd74af293f2b2
alym karim <akarim@openstudycollege.com>
2022-08-02 11:51:21 +0100

    fix: installed all original sh.files, created copies of modified files

--- .pscale/cli-helper-scripts/create-branch-connection-string-pr-branches.sh
+++ .pscale/cli-helper-scripts/create-branch-connection-string-pr-branches.sh
@@ -2,29 +2,17 @@ function create-branch-connection-string {
     local DB_NAME=$1
     local BRANCH_NAME=$2
     local ORG_NAME=$3
-    local CREDS=${4,,}
+    local CREDS=$4
     local secretshare=$5
 
-    # delete password if it already existed
-    # first, list password if it exists
-    local raw_output=`pscale password list "$DB_NAME" "$BRANCH_NAME" --org "$ORG_NAME" --format json `
-    # check return code, if not 0 then error
-    if [ $? -ne 0 ]; then
-        echo "Error: pscale password list returned non-zero exit code $?: $raw_output"
-        exit 1
-    fi
-
-    local output=`echo $raw_output | jq -r "[.[] | select(.display_name == \"$CREDS\") ] | .[0].id "`
-    # if output is not "null", then password exists, delete it
-    if [ "$output" != "null" ]; then
-        echo "Deleting existing password $output"
-        pscale password delete --force "$DB_NAME" "$BRANCH_NAME" "$output" --org "$ORG_NAME"
-        # check return code, if not 0 then error
-        if [ $? -ne 0 ]; then
-            echo "Error: pscale password delete returned non-zero exit code $?"
-            exit 1
-        fi
-    fi
+    # # delete password if it already existed
+    # # first, list password if it exists
+    # local raw_output=`pscale password list "$DB_NAME" "$BRANCH_NAME" --org "$ORG_NAME" --format json `
+    # # check return code, if not 0 then error
+    # if [ $? -ne 0 ]; then
+    #     echo "Error: pscale password list returned non-zero exit code $?: $raw_output"
+    #     exit 1
+    # fi
     
     local raw_output=`pscale password create "$DB_NAME" "$BRANCH_NAME" "$CREDS" --org "$ORG_NAME" --format json`
     
@@ -51,6 +39,8 @@ EOF
         local link=`curl -s -X POST -d "plain&secret=$SECRET_TEXT" https://shared-secrets-planetscale.herokuapp.com/`
         echo "$link"
         echo "::set-output name=CONNECTION_STRING_LINK::${link}"
+        export MY_DB_URL=$DB_URL
+        echo "MY_DB_URL=$DB_URL" >> $GITHUB_ENV
     fi
     echo
     echo "Alternatively, you can connect to your new branch like this:"
@@ -58,4 +48,5 @@ EOF
     echo "or, to create a local tunnel to the database:"
     echo "pscale connect \"$DB_NAME\" \"$BRANCH_NAME\" --org \"$ORG_NAME\""
     export MY_DB_URL=$DB_URL
+    echo "MY_DB_URL=$DB_URL" >> $GITHUB_ENV
 }
--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-. use-pscale-docker-image.sh
-
-. authenticate-ps.sh
-
 
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME"
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "pscale_dump_osc-academic_main"
