eced9485d5b29138b96be929e28f994aadcd041e
alym karim <akarim@openstudycollege.com>
2022-08-02 18:18:40 +0100

    fix: playing around with sudo

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+docker pull planetscale/pscale:latest
+mkdir -p $HOME/.config/planetscale
+alias pscale="docker run -e HOME=/tmp -v $HOME/.config/planetscale:/tmp/.config/planetscale --user $(id -u):$(id -g) --rm -it -p 3306:3306/tcp planetscale/pscale:latest"
+
 echo "Script location: ${DIR}"
-pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output "$DIR"
-pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "$DIR"
+sudo pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output "$DIR"
+sudo pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "$DIR"
