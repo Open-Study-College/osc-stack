20cc2af895d7759d7270117d32b335ee791e105a
alym karim <akarim@openstudycollege.com>
2022-08-02 18:05:17 +0100

    fix: sudo the dump-and-restore file

--- .pscale/cli-helper-scripts/create-db-branch-dr-and-connection-pr-branches.sh
+++ .pscale/cli-helper-scripts/create-db-branch-dr-and-connection-pr-branches.sh
@@ -27,4 +27,4 @@ create-branch-connection-string "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "testcred"
         done
     fi
 
-. .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "$DIR"
+sudo "$DIR/.pscale/cli-helper-scripts/dump-and-restore-db-branch.sh" "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "$DIR"
