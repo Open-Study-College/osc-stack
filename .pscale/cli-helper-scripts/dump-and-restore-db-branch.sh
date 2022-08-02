5d464006bd77ff5a4cb4c68d627275e6fc778354
alym karim <akarim@openstudycollege.com>
2022-08-02 17:10:35 +0100

    fix: dump to root

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 echo "Script location: ${DIR}"
-pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output "$DIR"
-pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "$DIR/pscale_dump_osc-academic_main"
+pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output "../../pscale_dump_osc-academic_main"
+pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "../../pscale_dump_osc-academic_main"
