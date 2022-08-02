6cfa05481c81c64a33b3cf2f71f13c5cc98dcfb2
alym karim <akarim@openstudycollege.com>
2022-08-02 12:15:15 +0100

    fix: do not use root dir

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pscale database dump "$DB_NAME" "main" --org "$ORG_NAME"
-pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "pscale_dump_osc-academic_main"
+pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output ".pscale/cli-helper-scripts"
+pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir ".pscale/cli-helper-scripts/pscale_dump_osc-academic_main"
