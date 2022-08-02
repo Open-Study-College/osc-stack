dff317cd7cd630dd99a395d1ea22f83be0864690
alym karim <akarim@openstudycollege.com>
2022-08-02 08:54:28 +0100

    fix: added sudo to commands in dump-and-restore-db-brancg

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir "pscale_dump_\"$DB_NAME\"_\"$BRANCH_NAME\""
-pscale database dump "$DB_NAME" "main" --org "$ORG_NAME"
-pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "pscale_dump_\"$DB_NAME\"_\"$BRANCH_NAME\""
+sudo mkdir "pscale_dump_\"$DB_NAME\"_\"$BRANCH_NAME\""
+sudo pscale database dump "$DB_NAME" "main" --org "$ORG_NAME"
+sudo pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "pscale_dump_\"$DB_NAME\"_\"$BRANCH_NAME\""
