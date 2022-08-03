16ece7678b7a221e93c31f58d856ee1a30d6aff7
alym karim <akarim@openstudycollege.com>
2022-08-03 11:34:54 +0100

    fix: replaced /tmp with /run

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tmpfolder="/tmp/main"
+tmpfolder="/run/main"
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output $tmpfolder
-cp -v -a /tmp/* $HOME
+cp -v -a /run/* $HOME
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir $HOME
