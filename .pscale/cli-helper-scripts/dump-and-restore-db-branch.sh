bbba7261c2ec641afcfc9053e7edb4bc08ecefe3
alym karim <akarim@openstudycollege.com>
2022-08-03 11:44:10 +0100

    fix: write files to /var/run

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-tmpfolder="/run/main"
+tmpfolder="/var/run"
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output $tmpfolder
-cp -v -a /run/* $HOME
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir $HOME
