d59d8631820713c10c7c15bf86896ba46e95320c
alym karim <akarim@openstudycollege.com>
2022-08-03 09:53:00 +0100

    fix: added conditional to restore dump occuring

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -2,5 +2,4 @@
 
 echo "Script location: ${DIR}"
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output "/var/tmp/main"
-find /tmp/main -type d
-pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "/var/tmp/main"
+&& pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "/var/tmp/main"
