86966f875736025d3449a7d49ed66eaf53619385
alym karim <akarim@openstudycollege.com>
2022-08-03 09:58:38 +0100

    fix: syntax for tmp folder

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -2,5 +2,5 @@
 
 echo "Script location: ${DIR}"
 tmpfolder=$(mktemp -d)
-pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output tmpfolder
-pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir tmpfolder
+pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output $tmpfolder
+pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir $tmpfolder
