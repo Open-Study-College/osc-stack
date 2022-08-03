493e36afdcc43228a743279ca3713829d227a9c1
alym karim <akarim@openstudycollege.com>
2022-08-03 08:09:50 +0100

    fix: create subdirectory inside tmp dir

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Script location: ${DIR}"
-pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output "/tmp"
-pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "/tmp"
+pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output "/tmp/main"
+pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "/tmp/main"
