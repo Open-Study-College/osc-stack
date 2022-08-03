6b35cc53838ab626a9903aeb7184498f4c03b6c7
alym karim <akarim@openstudycollege.com>
2022-08-03 08:50:06 +0100

    fix: new echo script

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -2,11 +2,7 @@
 
 echo "Script location: ${DIR}"
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output "/tmp/main"
-
-search_dir=/tmp/main
-for entry in "$search_dir/*"
-do
-  echo "$entry"
+for d in /tmp/main ; do
+    echo "$d"
 done
-
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "/tmp/main"
