46664ac0fbb41569a70889fea0f01ea6c0759ede
alym karim <akarim@openstudycollege.com>
2022-08-03 09:42:07 +0100

    fix: restore directory in var/tmp/main

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Script location: ${DIR}"
-pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output "$HOME"
+pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output "/var/tmp/main"
 find /tmp/main -type d
-pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "$HOME"
+pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "/var/tmp/main"
