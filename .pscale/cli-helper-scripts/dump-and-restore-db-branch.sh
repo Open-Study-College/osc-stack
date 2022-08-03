a817b0c7db3500a91d3336eaff5f5c10d133e828
alym karim <akarim@openstudycollege.com>
2022-08-03 11:27:06 +0100

    fix: verbose flag on copy command

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -2,5 +2,5 @@
 
 tmpfolder="/tmp/main"
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output $tmpfolder
-cp -a /tmp/* $HOME
+cp -v -a /tmp/* $HOME
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir $HOME
