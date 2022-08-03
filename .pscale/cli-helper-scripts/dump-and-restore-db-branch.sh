34676015eae7ec1c1eb15f94c115dc4e2ba502a3
alym karim <akarim@openstudycollege.com>
2022-08-03 09:56:10 +0100

    fix: dump and restore to tmp folder

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Script location: ${DIR}"
-pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output "/var/tmp/main"
-&& pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir "/var/tmp/main"
+tmpfolder=$(mktemp -d)
+pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output tmpfolder
+pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir tmpfolder
