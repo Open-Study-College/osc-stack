c77d0d4267e7cac6e76f6d9a9f23d67da631c297
alym karim <akarim@openstudycollege.com>
2022-08-03 14:46:59 +0100

    fix: list all sql files

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -2,5 +2,5 @@
 
 tmpfolder=$DIR
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output $tmpfolder/tmpdir
-ls -la $tmpfolder
+ls -la $tmpfolder/tmpdir
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir $tmpfolder/tmpdir
