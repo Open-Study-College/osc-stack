a8b1bf4f99d0f6ef6317cfa5d650cd8a66dc9148
alym karim <akarim@openstudycollege.com>
2022-08-03 10:20:37 +0100

    fix: restore dump from run/main

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tmpfolder="/run"
+tmpfolder="/run/main"
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output $tmpfolder
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir $tmpfolder
