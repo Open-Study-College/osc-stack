358b64b1130b727957a914a37e397a796fcb8b62
alym karim <akarim@openstudycollege.com>
2022-08-03 12:09:26 +0100

    fix: echo and list home dir

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 tmpfolder="/tmp/main"
-ls -la $tmpfolder
+echo $HOME
+ls -la $HOME
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output $tmpfolder
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir $tmpfolder
