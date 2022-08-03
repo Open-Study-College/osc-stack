50c821a036333292f8db61b5da4e0b12dc234199
alym karim <akarim@openstudycollege.com>
2022-08-03 11:00:24 +0100

    fix: update use-pscale-docker-image

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mkdir -p /var/tmp/main
-tmpfolder="/var/tmp/main"
+tmpfolder="/tmp/main"
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output $tmpfolder
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir $tmpfolder
