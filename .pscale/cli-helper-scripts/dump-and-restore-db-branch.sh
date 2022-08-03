a1dfe2405eca5db75e0d07c60b2ec64ce78ae01d
alym karim <akarim@openstudycollege.com>
2022-08-03 14:00:34 +0100

    fix: list contents of tmpfolder

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 tmpfolder=$DIR
-echo $HOME
-ls -la $HOME
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output $tmpfolder
+ls -la $tmpfolder
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir $tmpfolder
