cf7b204096267e34ccf752e80fa9f8bed158596a
alym karim <akarim@openstudycollege.com>
2022-08-03 10:46:36 +0100

    fix: changed tmp to var/tmp

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p /tmp/main
-tmpfolder="/tmp/main"
+mkdir -p /var/tmp
+tmpfolder="/var/tmp"
 pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output $tmpfolder
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir $tmpfolder
