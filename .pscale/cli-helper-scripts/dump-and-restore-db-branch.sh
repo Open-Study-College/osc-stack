027ea647f91c51e54592e43c57649151b8ada1b0
alym karim <akarim@openstudycollege.com>
2022-08-04 11:25:29 +0100

    fix: rmv excess files, fix connection string. restore from staging, added user.name to schema

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 tmpfolder=$DIR
-pscale database dump "$DB_NAME" "main" --org "$ORG_NAME" --output $tmpfolder/tmpdir
+pscale database dump "$DB_NAME" "staging" --org "$ORG_NAME" --output $tmpfolder/tmpdir
 ls -la $tmpfolder/tmpdir
 pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir $tmpfolder/tmpdir
