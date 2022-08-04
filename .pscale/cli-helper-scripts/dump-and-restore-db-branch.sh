bc321dc993e2e09e142faf4db4e95f39e1c9b7f6
alym karim <akarim@openstudycollege.com>
2022-08-04 16:04:58 +0100

    fix: finish tidying planetscale pr

--- .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
+++ .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tmpfolder=$DIR
-pscale database dump "$DB_NAME" "staging" --org "$ORG_NAME" --output $tmpfolder/tmpdir
-ls -la $tmpfolder/tmpdir
-pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir $tmpfolder/tmpdir
+tmpfolder=$DIR/tmp
+pscale database dump "$DB_NAME" "staging" --org "$ORG_NAME" --output $tmpfolder
+ls -la $tmpfolder
+pscale database restore-dump  "$DB_NAME" "$BRANCH_NAME" --overwrite-tables --org "$ORG_NAME" --dir $tmpfolder
