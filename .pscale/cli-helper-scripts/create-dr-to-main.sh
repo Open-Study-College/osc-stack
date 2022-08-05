b07429219cf73448ab7c854248df692e4e87c3e9
alym karim <akarim@openstudycollege.com>
2022-08-05 02:55:14 +0100

    fix: added merge step when main changes

--- .pscale/cli-helper-scripts/create-dr-to-main.sh
+++ .pscale/cli-helper-scripts/create-dr-to-main.sh
@@ -11,3 +11,5 @@ DDL_STATEMENTS="$2"
 . .pscale/cli-helper-scripts/ps-create-helper-functions-pr-branches.sh
 create-schema-change "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "$DDL_STATEMENTS"
 create-deploy-request "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME"
+
+. .pscale/cli-helper-scripts/merge-deploy-request.sh "$DB_NAME" "$BRANCH_NAME" "$DEPLOY_REQUEST_NUMBER"
