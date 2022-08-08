2616577e865acd80db9f4360fec1c0ba742526b9
alym karim <akarim@openstudycollege.com>
2022-08-08 16:17:17 +0100

    fix: move wait_for_deploy_request_merged to helper functions

--- .pscale/cli-helper-scripts/create-dr-to-main.sh
+++ .pscale/cli-helper-scripts/create-dr-to-main.sh
@@ -11,4 +11,7 @@ DDL_STATEMENTS="$2"
 . .pscale/cli-helper-scripts/ps-create-helper-functions-pr-branches.sh
 create-schema-change "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "$DDL_STATEMENTS"
 create-deploy-request "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME"
+
+echo $DEPLOY_REQUEST_NUMBER
+
 create-deployment "$DB_NAME" "$ORG_NAME" "$DEPLOY_REQUEST_NUMBER"
