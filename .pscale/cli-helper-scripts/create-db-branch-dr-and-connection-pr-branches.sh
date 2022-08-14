4b13abb5960003acae1653db6b27ee9493617c1b
alym karim <akarim@openstudycollege.com>
2022-08-14 17:15:09 +0100

    fix: tidying up db-pr

--- .pscale/cli-helper-scripts/create-db-branch-dr-and-connection-pr-branches.sh
+++ .pscale/cli-helper-scripts/create-db-branch-dr-and-connection-pr-branches.sh
@@ -5,14 +5,9 @@
 
 . .pscale/cli-helper-scripts/authenticate-ps.sh
 
-BRANCH_NAME="$1"
-DDL_STATEMENTS="$2" 
-
-. .pscale/cli-helper-scripts/set-db-and-org-and-branch-name.sh
-
 . .pscale/cli-helper-scripts/ps-create-helper-functions-pr-branches.sh
-create-db-branch "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "recreate" "$FROM"
+create-db-branch "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "recreate" "$DIR"
 
-. .pscale/cli-helper-scripts/create-branch-connection-string-pr-branches.sh
+. .pscale/cli-helper-scripts/create-branch-connection-string-pr-branches.sh "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "${BRANCH_NAME}" 
 
 . .pscale/cli-helper-scripts/dump-and-restore-db-branch.sh "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "$DIR"
