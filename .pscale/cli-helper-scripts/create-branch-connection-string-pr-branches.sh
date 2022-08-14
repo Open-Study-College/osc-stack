9f6115c4e3b6027ecd582b5bd6d60926f956c7c1
alym karim <akarim@openstudycollege.com>
2022-08-14 14:51:41 +0100

    fix: run function after declaring it

--- .pscale/cli-helper-scripts/create-branch-connection-string-pr-branches.sh
+++ .pscale/cli-helper-scripts/create-branch-connection-string-pr-branches.sh
@@ -62,3 +62,13 @@ EOF
     export MY_DB_URL=$DB_URL
     echo "MY_DB_URL=$DB_URL" >> $GITHUB_ENV
 }
+
+create-branch-connection-string "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "testcred"
+    # if $2 and $3 are set, generate secret output links
+    if [ -n "$2" ] && [ -n "$3" ]; then
+        for i in `seq 1 $2`; do
+            for j in `seq 1 $3`; do
+                echo "::set-output name=dbconnection_${i}_${j}::`curl -s -X POST -d "plain&secret=$MY_DB_URL" https://shared-secrets-planetscale.herokuapp.com/`"          
+            done
+        done
+    fi
