0e82fdfda79408af5517545aa963b6fd29a604d8
alym karim <akarim@openstudycollege.com>
2022-08-15 09:04:37 +0100

    fix: tidy up

--- .pscale/cli-helper-scripts/delete-old-password.sh
+++ .pscale/cli-helper-scripts/delete-old-password.sh
@@ -1,8 +1,8 @@
-function create-branch-connection-string {
+function delete-branch-connection-string {
     local DB_NAME=$1
     local BRANCH_NAME=$2
     local ORG_NAME=$3
-    local CREDS=${4,,}-PR-$(uuidgen)
+    local CREDS=${4,,}-CICD-
     local secretshare=$5
 
     # delete password if it already existed
@@ -14,8 +14,9 @@ function create-branch-connection-string {
         exit 1
     fi
 
-    local output=`echo $raw_output | jq -r "[.[] | select(.display_name.startswith(\"$CREDS\"-PR-)) ] | .[1].id "`
+    local output=`echo $raw_output | jq -r "[.[] | select(.display_name.startswith(\"$CREDS\")) ] | .[1].id "`
     # if output is not "null", then password exists, delete it
+
     if [ "$output" != "null" ]; then
         echo "Deleting existing password $output"
         pscale password delete --force "$DB_NAME" "$BRANCH_NAME" "$output" --org "$ORG_NAME"
@@ -33,12 +34,4 @@ function create-branch-connection-string {
 
 . .pscale/cli-helper-scripts/authenticate-ps.sh
 
-create-branch-connection-string "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "${BRANCH_NAME}" 
-    # if $2 and $3 are set, generate secret output links
-    if [ -n "$2" ] && [ -n "$3" ]; then
-        for i in `seq 1 $2`; do
-            for j in `seq 1 $3`; do
-                echo "::set-output name=dbconnection_${i}_${j}::`curl -s -X POST -d "plain&secret=$MY_DB_URL_MAIN" https://shared-secrets-planetscale.herokuapp.com/`"          
-            done
-        done
-    fi
+delete-branch-connection-string "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "${BRANCH_NAME}" 
