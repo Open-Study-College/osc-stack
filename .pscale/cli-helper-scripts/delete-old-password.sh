4863d9faaa59f1a134c3038d1bfde8ad66d96422
alym karim <akarim@openstudycollege.com>
2022-08-15 10:52:18 +0100

    fix: syntax error

--- .pscale/cli-helper-scripts/delete-old-password.sh
+++ .pscale/cli-helper-scripts/delete-old-password.sh
@@ -14,7 +14,7 @@ function delete-branch-connection-string {
         exit 1
     fi
 
-    local output=`echo $raw_output | jq -r "[.[] | select(.display_name | startswith(\"$CREDS\")) | join(",") ]"`
+    local output=`echo $raw_output | jq -r "[.[] | select(.display_name | startswith(\"$CREDS\") | join(",") ) ]"`
     # if output is not "null", then password exists, delete it
 
     for row in $(echo "${output}"); do
